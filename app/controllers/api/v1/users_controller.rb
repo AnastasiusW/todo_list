df6d02e7ca589d8d2cdd59294fa7e3b2992e6e0b
AnastasiusW <repositoryanastasiia@gmail.com>
2020-07-08 20:16:50 +0300

    Add auth controller and test

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,10 +3,9 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: user, status: :created
+      render json: UserSerializer.new(user).serialized_json, status: :created
     else
-      render json:user.errors.full_messages,
-             status: :unprocessable_entity
+      render json: user.errors, status: :unprocessable_entity
     end
   end
 
--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -2,16 +2,27 @@
 RSpec.describe "Api::V1::Users", type: :request do
 
   describe 'POST api/v1/users' do
-    let(:user_attributes) { attributes_for(:user) }
-    let(:user_params) {user_attributes.merge(password_confirmation: user_attributes[:password])}
+
     before do
       post '/api/v1/users', params: user_params
     end
     context 'when input valid params for register new user' do
+      let(:user_attributes) { attributes_for(:user) }
+      let(:user_params) {user_attributes.merge(password_confirmation: user_attributes[:password])}
       it 'sign up user with success' do
         expect(response.body).to match_json_schema('user')
 
         expect(response).to have_http_status(:created)
+
+      end
+    end
+
+    context 'when input invalid params for register new user' do
+      let(:user_params) {{user_name: '', password:'password', password_confirmation:'wrong'}}
+      it 'sign up user with empty user_name and wrong password_confirmation' do
+
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(User.count).to eq(0)
       end
     end
   end
